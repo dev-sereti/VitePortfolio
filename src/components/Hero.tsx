d0627f27bb69f93f7dd48915ea817a2edb8ee94a
dev <devsereti@gmail.com>
2025-08-04 18:52:12 +0300

    GitHub added.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -116,7 +116,7 @@ const Hero: React.FC = () => {
               className="flex gap-6 justify-center lg:justify-start"
             >
               {[
-                { icon: Github, href: '', label: 'GitHub' },
+                { icon: Github, href: 'https://github.com/dev-sereti  ', label: 'GitHub' },
                 { icon: Linkedin, href: 'https://www.linkedin.com/in/dev-sereti/', label: 'LinkedIn' },
                 { icon: Mail, href: 'mailto:seretikelvin25@gmail.com', label: 'Email' },
                 { icon: Phone, href: 'tel:+254792323000', label: 'Phone' },
